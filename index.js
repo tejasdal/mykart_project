db31482bb62e80f038b165efb8cfe49a04866e16
Tejas Patel <tejas.patel@dal.ca>
2020-08-07 20:21:51 -0300

    Added API to get products from My Seller.

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const axios = require('axios');
 
 const serverless = require('serverless-http');
 
+const MY_SELLER_URL = "http://192.168.0.12:1337";
+const MY_DELIVERY_URL = "";
+
 //database connection
 var con = mysql.createConnection({
     host: "assignment4.ckyqzu90h1yu.us-east-1.rds.amazonaws.com",
@@ -43,6 +46,39 @@ app.post('/login', function (req, res) {
 });
 
 
+//Fetch products from MySeller
+app.get('/products', function(req, res){
+
+    return getProductsFromMySeller(res);
+});
+
+app.post('/order', function(req, res){
+
+    
+});
+
+async function getProductsFromMySeller(res) {
+
+    console.log('Fetching parts list from My Seller.');
+    try {
+      let response = await axios.get( MY_SELLER_URL+'/products/get-all-products');
+      if (response.status === 200) {
+
+        res.send(response.data);
+        return;
+      }
+    } catch (err) {
+      console.log('Error while fetching products from My Seller, error: ' + err);
+      sendError(res,'Error while fetching products from My Seller: ' + err.response.data);
+      return null;
+    }
+  }
+
+  function sendError(res, message) {
+    res.status(500).send({
+      message: message
+    });
+  }
 
 //App listening on PORT 3000
 app.listen(3000);
