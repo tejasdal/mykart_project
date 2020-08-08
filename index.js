3863379e1df0daac02bf16bd57829efc0131fce2
Roshan Kirtikumar Patel <rs735202@dal.ca>
2020-08-08 15:31:59 -0300

    Added API to inform seller and delivery company about the placed order

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ con.connect(function (err) {
     console.log("Database is successfully Connected!");
 });
 
-
 //User validation (Will be used when the user wants to place the order)
 app.post('/login', function (req, res) {
     var emailid = req.body.emailid;
@@ -33,7 +32,7 @@ app.post('/login', function (req, res) {
 
     //checking if any value is null or not
     if (emailid === "" || password === "" || !emailid || !password) {
-        res917.status(412).send("please enter all the required input ");
+        res.status(412).send("please enter all the required input ");
     }
     con.query("SELECT * FROM user WHERE emailid=? and password=?", [emailid, password], (err, rows, fields) => {
         if (!err) {
@@ -95,36 +94,6 @@ app.get('/products', function (req, res) {
     return getProductsFromMySeller(res);
 });
 
-app.post('/order', function (req, res) {
-
-    const schema = Joi.object({
-        userId: Joi.number().required(),
-        sellerId: Joi.number().required(),
-        orderQty: Joi.number().min(1).required(),
-        productId: Joi.number().required(),
-        userAdd: Joi.string().required(),
-        orderTotal: Joi.number().required(),
-    });
-
-    const result = schema.validate(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    let sql = `INSERT INTO Orders(user_id, seller_id, order_qty, product_id, user_address, order_total) VALUES (?, ?, ?, ?, ?, ?)`;
-
-    con.query(sql, [req.body.userId, req.body.sellerId, req.body.orderQty, req.body.productId, req.body.userAdd, req.body.orderTotal], (err,result) =>{
-        if(err){
-            console.log(err);
-            res.status(500).send("DB Error while adding orders.");
-            return;
-        }
-        res.status(200).send(req.body);
-    });
-
-
-});
 
 async function getProductsFromMySeller(res) {
 
@@ -143,10 +112,171 @@ async function getProductsFromMySeller(res) {
     }
 }
 
+
 function sendError(res, message) {
     res.status(500).send({
         message: message
     });
+}
+
+
+
+//Storing the order information in the database
+app.post('/order', function (req, res) {
+
+    const schema = Joi.object({
+        userId: Joi.number().required(),
+        sellerId: Joi.number().required(),
+        orderQty: Joi.number().min(1).required(),
+        productId: Joi.number().required(),
+        userAdd: Joi.string().required(),
+        orderTotal: Joi.number().required(),
+    });
+
+    const result = schema.validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    //Calling async function to check the qty from the seller.
+    checkQtyFromSeller(req);
+
+
+
+});
+
+
+//Async function to call the seller api and confirm that the ordered product's qty is available or not
+async function checkQtyFromSeller(req) {
+    let promise = await confirmQtyFromSeller(req);
+    if (promise == true) {
+        storeOrderData(req);
+    }
+}
+
+//Async function that calls the seller API to check the qty
+async function confirmQtyFromSeller(req) {
+
+    console.log('Calling the API seller company to check the Quantity of the selected product');
+    try {
+        let response = await axios.get(MY_SELLER_URL + '/confirmQuantity',
+            {
+                "seller_id": req.body.sellerId,
+                "product_id": req.body.productId
+            });
+        if (response.status === 200) {
+            return true;
+        }
+    } catch (err) {
+        console.log('Error while storing the  order data into the Delivery company: ' + err);
+        sendError(res, 'Error while storing the  order data into the Delivery company: ' + err.response.data);
+        return false;
+    }
+}
+
+
+//If the qty is available then store the irder data into the MyKart's database.
+async function storeOrderData(req) {
+
+
+    console.log("Storing the user order data in to MyKArt company's Database")
+    let sql = `INSERT INTO Orders(user_id, seller_id, order_qty, product_id, user_address, order_total) VALUES (?, ?, ?, ?, ?, ?)`;
+
+    con.query(sql, [req.body.userId, req.body.sellerId, req.body.orderQty, req.body.productId, req.body.userAdd, req.body.orderTotal], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("DB Error while adding orders.");
+            return;
+        }
+        else {
+            //Getting the last Order_id
+            con.query("select order_id from orders where order_id=(SELECT LAST_INSERT_ID())", async (err, rows, fields) => {
+                if (!err) {
+                    //Getting the order_id
+                    let order_id = rows[0].order_id;
+                    console.log("user's OrderId is-->" + order_id)
+                    //waiting for the delivery company's response (Calling them to store the order data)
+                    let promise = await storeOrderDataInDeliverCompany(req, order_id);
+                    if (promise == true) {
+                        //Calling seller company to store the order data
+                        storeOrderDataInSellerCompany(req, order_id);
+                        res.status(200).send(req.body);
+                    }
+                }
+                else {
+                    res.status(500).send("Server error please try again later");
+                }
+            });
+        }
+    });
+}
+//Call the delivercompany API and send the order information(Storing the order data).
+async function storeOrderDataInDeliverCompany(req, order_id) {
+    console.log('Caaling the API of Delivery company and storing the ordered details');
+    try {
+        let response = await axios.get(MY_DELIVERY_URL + '/orderData',
+            {
+                "user_id": req.body.userId,
+                "seller_id": req.body.sellerId,
+                "order_qty": req.body.orderQty,
+                "product_id": req.body.productId,
+                "user_address": req.body.userAdd,
+                "order_total": req.body.orderTotal,
+                "order_id": order_id
+            });
+        if (response.status === 200) {
+            return true;
+        }
+    } catch (err) {
+        console.log('Error while storing the  order data into the Delivery company: ' + err);
+        sendError(res, 'Error while storing the  order data into the Delivery company: ' + err.response.data);
+        return false;
+    }
+}
+
+
+
+//Call the Seller comapny API and send the order information(Storing the order data).
+async function storeOrderDataInSellerCompany(req, order_id) {
+    console.log('Caaling the API of Delivery company and storing the ordered details');
+    try {
+        let response = await axios.get(MY_SELLER_URL + '/orderData',
+            {
+                "user_id": req.body.userId,
+                "seller_id": req.body.sellerId,
+                "order_qty": req.body.orderQty,
+                "product_id": req.body.productId,
+                "user_address": req.body.userAdd,
+                "order_total": req.body.orderTotal,
+                "order_id": order_id
+            });
+        if (response.status === 200) {
+            return true;
+        }
+    } catch (err) {
+        console.log('Error while storing the  order data into the Delivery company: ' + err);
+        sendError(res, 'Error while storing the  order data into the Delivery company: ' + err.response.data);
+        return false;
+    }
+}
+
+
+
+app.get('/temp', function (req, res) {
+    let data = A(req);
+
+})
+
+async function A(params) {
+    let data = await B(params);
+    console.log(data)
+
+}
+
+
+async function B(params) {
+    return false;
 }
 
 //App listening on PORT 3000
