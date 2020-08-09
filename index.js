60d2f8111a51a68e951af32f8e754ffe40506222
Roshan Kirtikumar Patel <rs735202@dal.ca>
2020-08-09 11:38:46 -0300

    Updated URL of delivery company to access the API

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const serverless = require('serverless-http');
 
 // const MY_SELLER_URL = "http://192.168.0.12:1337";
 const MY_SELLER_URL = "http://localhost:8080";
-const MY_DELIVERY_URL = "http://localhost:8848";
+const MY_DELIVERY_URL = "http://ec2-18-212-133-17.compute-1.amazonaws.com:1337";
 
 //database connection
 var con = mysql.createConnection({
